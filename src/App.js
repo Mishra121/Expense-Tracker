5391ec97509f035f34527bd3c1a5d8e63de3e12b
Vibhu <vibhu98mishra@gmail.com>
2020-07-06 13:19:50 +0530

    Some syntax changes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {Header } from './components/Header';
-import {Balance } from './components/Balance';
-import {IncomeExpenses} from './components/IncomeExpenses';
-import { TransactionList } from "./components/TransactionList";
+import Header  from './components/Header';
+import Balance  from './components/Balance';
+import IncomeExpenses from './components/IncomeExpenses';
+import TransactionList from "./components/TransactionList";
 
 import './App.css';
 
