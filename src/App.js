2044f89c45f4730104f28921a729b70df93f0acd
Vibhu <vibhu98mishra@gmail.com>
2020-07-06 13:21:56 +0530

    Add Transaction UI done

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import Header  from './components/Header';
 import Balance  from './components/Balance';
 import IncomeExpenses from './components/IncomeExpenses';
 import TransactionList from "./components/TransactionList";
-
-import './App.css';
+import AddTransaction from './components/AddTransaction'
 
 function App() {
   return (
@@ -14,6 +13,7 @@ function App() {
         <Balance/>
         <IncomeExpenses/>
         <TransactionList />
+        <AddTransaction/>
       </div>
     </div>
   );
